accf06d89ffecd7d3672b968027272bf72c5ef79
高村美紀 <takamuraminori@koumuramikinoMacBook-Pro.local>
2020-08-29 14:11:31 +0900

    画像読み込みチェック

--- js/index.js
+++ js/index.js
@@ -1,84 +1,95 @@
 window.addEventListener("DOMContentLoaded", init);
 
-
-
 function init() {
-  const width = 960;
-  const height = 540;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
 
   // レンダラーを作成
   const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector("#myCanvas")
+    canvas: document.querySelector("#myCanvas"),
   });
-  renderer.setClearColor( 0xdc6b9a, 1.3 ); // 背景色
+  renderer.setClearColor(0xdc6b9a, 1.3); // 背景色
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(width, height);
 
   // シーンを作成
-  
+
   const scene = new THREE.Scene();
 
   // カメラを作成
-  const camera = new THREE.PerspectiveCamera(
-    45,
-    width / height,
-    1,
-    10000
-  );
+  const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
   camera.position.set(0, 0, +1000);
 
   // 箱を作成
-  const geometry = new THREE.BoxGeometry(400, 400, 400);
+  const geometry = new THREE.BoxGeometry(250, 250, 250);
 
   let loader = new THREE.TextureLoader();
-  let texture = loader.load( '../logo.jpg' );
-  let material = new THREE.MeshBasicMaterial( { map: texture } );
-    // var materials = [
-    //     new THREE.MeshLambertMaterial({color: 0x2cb4ad}),
-    //     new THREE.MeshLambertMaterial({color: 0x2cb4ad}),
-    //     new THREE.MeshLambertMaterial({color: 0xdc6b9a}),
-    //     new THREE.MeshLambertMaterial({color: 0xdc6b9a}),
-    //     new THREE.MeshLambertMaterial({color: 0x68a4d9}),
-    //     new THREE.MeshLambertMaterial({color: 0x68a4d9})
-    // ];
-    // var material = new THREE.MeshFaceMaterial(materials);
+  let texture = loader.load("../logo.jpg");
+  // let material = new THREE.MeshBasicMaterial( { map: texture } );
+  let materials = [
+    new THREE.MeshBasicMaterial({ map: loader.load("Ai_icon.png") }),
+    new THREE.MeshBasicMaterial({ map: loader.load("logo.jpg") }),
+    new THREE.MeshBasicMaterial({ map: loader.load("Ai_icon.png") }),
+    new THREE.MeshBasicMaterial({ map: loader.load("logo.jpg") }),
+    new THREE.MeshBasicMaterial({ map: loader.load("Ai_icon.png") }),
+    new THREE.MeshBasicMaterial({ map: loader.load("logo.jpg") }),
+  ];
+  // var materials = [
+  //   new THREE.MeshLambertMaterial({ color: 0x2cb4ad }),
+  //   new THREE.MeshLambertMaterial({ color: 0x2cb4ad }),
+  //   new THREE.MeshLambertMaterial({ color: 0xdc6b9a }),
+  //   new THREE.MeshLambertMaterial({ color: 0xdc6b9a }),
+  //   new THREE.MeshLambertMaterial({ color: 0x68a4d9 }),
+  //   new THREE.MeshLambertMaterial({ color: 0x68a4d9 })
+  // ];
+  var material = new THREE.MeshFaceMaterial(materials);
 
   const box = new THREE.Mesh(geometry, material);
   scene.add(box);
 
   // 平行光源
   const light = new THREE.DirectionalLight(0xffffff);
-  light.intensity = 1.5; // 光の強さを倍に
-  light.position.set(0, 0,10);
+  light.intensity = 1.0; // 光の強さを倍に
+  light.position.set(0, 0, 10);
   // シーンに追加
   scene.add(light);
 
-//   初回実行
+  //   初回実行
   tick();
 
   function tick() {
+    window.onmousedown = handleMouseDown;
     window.onmousemove = handleMouseMove;
+    window.onmouseup = handleMouseUp;
     requestAnimationFrame(tick);
 
+    function handleMouseDown(event) {
+      this.isMouseDown = true;
+    }
     function handleMouseMove(event) {
-        b_x = width/2;
-        b_y = height/2;
-        event = event || window.event; // IE対応
-        if(event.clientX < 960 && event.clientY < 540){
-            x = parseInt(event.clientX) * 0.005;
-            y = (parseInt(event.clientY)- 250) * 0.005;
-            box.rotation.x = y - b_y;
-            box.rotation.y = x - b_x;
-        }
-        b_x = x
-        b_y = y
-        // console.log(String(x) + ", " + String(y));
+      if (!this.isMouseDown && width < 480) {
+        return;
+      }
+      b_x = width / 2;
+      b_y = height / 2;
+      event = event || window.event; // IE対応
+      if (event.clientX < width && event.clientY < height) {
+        x = parseInt(event.clientX) * 0.005;
+        y = (parseInt(event.clientY) - 50) * 0.005;
+        box.rotation.x = y - b_y;
+        box.rotation.y = x - b_x;
+      }
+      // console.log(x + "," + y);
+      b_x = x;
+      b_y = y;
+      // console.log(String(x) + ", " + String(y));
     }
     // box.rotation.x += 0.01;
     // box.rotation.y += 0.01;
-
     // レンダリング
     renderer.render(scene, camera);
   }
-
+  function handleMouseUp(event) {
+    this.isMouseDown = false;
+  }
 }
